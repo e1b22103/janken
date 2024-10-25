a73e0737f91708c4edd4d1c8887990707e5c004b
e1b22103 <e1b22103@oit.ac.jp>
2024-10-25 14:37:40 +0900

    DBの初期化処理を実装する

--- src/main/java/oit/is/z2421/kaizi/janken/model/MatchMapper.java
+++ src/main/java/oit/is/z2421/kaizi/janken/model/MatchMapper.java
@@ -10,10 +10,10 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface MatchMapper {
 
-  @Select("SELECT id,user1,user2,user1Hand,user2Hand from matches")
+  @Select("SELECT id,user1,user2,user1Hand,user2Hand,isActive from matches")
   ArrayList<Match> selectAllData();
 
-  @Insert("INSERT INTO matches (user1,user2,user1Hand,user2Hand) VALUES (#{user1},#{user2},#{user1Hand},#{user2Hand})")
+  @Insert("INSERT INTO matches (user1,user2,user1Hand,user2Hand,isActive) VALUES (#{user1},#{user2},#{user1Hand},#{user2Hand})")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
   void insertMatch(Match match);
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 INSERT INTO users (name) VALUES ('CPU');
 INSERT INTO users (name) VALUES ('ほんだ');
-INSERT INTO matches (user1,user2,user1Hand,user2Hand) VALUES (2,1,'rock','scissors');
-INSERT INTO matches (user1,user2,user1Hand,user2Hand) VALUES (2,1,'rock','rock');
-INSERT INTO matches (user1,user2,user1Hand,user2Hand) VALUES (2,1,'rock','paper');
+INSERT INTO users (name) VALUES ('いがき');
+INSERT INTO matchinfo (user1,user2,user1Hand,isActive) VALUES (1,2,'rock','FALSE');
+INSERT INTO matchinfo (user1,user2,user1Hand,isActive) VALUES (2,3,'scissors','FALSE');
+INSERT INTO matches (user1,user2,user1Hand,user2Hand,isActive) VALUES (2,1,'rock','scissors','FALSE');
+INSERT INTO matches (user1,user2,user1Hand,user2Hand,isActive) VALUES (2,1,'rock','rock','FALSE');
+INSERT INTO matches (user1,user2,user1Hand,user2Hand,isActive) VALUES (2,1,'rock','paper','FALSE');
