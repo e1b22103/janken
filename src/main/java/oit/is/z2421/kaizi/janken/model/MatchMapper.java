85d8339faa2f971f626c5abf08fed3eba3ab64f4
e1b22103 <e1b22103@oit.ac.jp>
2024-10-27 18:17:40 +0900

    対戦相手が手を選び非同期処理で結果を表示する

--- src/main/java/oit/is/z2421/kaizi/janken/model/MatchMapper.java
+++ src/main/java/oit/is/z2421/kaizi/janken/model/MatchMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface MatchMapper {
@@ -13,8 +14,14 @@ public interface MatchMapper {
   @Select("SELECT id,user1,user2,user1Hand,user2Hand,isActive from matches")
   ArrayList<Match> selectAllData();
 
+  @Select("SELECT id,user1,user2,user1Hand,user2Hand from matches where isActive=true")
+  Match selectByIsActive();
+
   @Insert("INSERT INTO matches (user1,user2,user1Hand,user2Hand,isActive) VALUES (#{user1},#{user2},#{user1Hand},#{user2Hand},#{isActive})")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
   void insertMatch(Match match);
+
+  @Update("UPDATE matches set isActive=false where id = #{id}")
+  void updateById(int id);
 
 }
